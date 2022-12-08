fa57fd2c268a58be0650d976d85ac390256a3fbc
gilangmgm98 <gilangmgm98@gmail.com>
2022-12-08 13:25:51 +0700

    database

---
+++ database.js
@@ -0,0 +1,289 @@
+const dbPria = [
+    {
+        nama: "Arvin Santoso",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "Apapun hasil yg penting barium fox 54 lulus Lanjut phase sampai kelar ",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Jhonfriadi Aritonang",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "semakin semangat untuk belajar setiap harinya",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Mochamad Alfin Rakhmat Fadillah",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "terus berusaha untuk belajar",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Roihan Salsabila",
+        role: "student",
+        umur: 21,
+        "kalimat untuk hacktive": "dekati semua yang bisa mengajari",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Hamzah Diza Santoso",
+        role: "student",
+        umur: 23,
+        "kalimat untuk hacktive": "be humble and chill",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Alfian Elsa Arief",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "berusaha dan belajar",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Jabbar Akbar",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "pantang menyerah adalah wajib",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Dyno Christhoper Marthin",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "tetap berusaha menyelesaikan",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Cakra Jonatan",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "Gg coding",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Muhammad Daffa Ashshiddiq",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "mantepin aja ",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Bagas Gunawan",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "tuntaskan apa yang udah dimulai",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Fahmi Maulana",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "chill aja",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Okta Falova",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "tetep berusaha aja",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Muhammad Dicky Taruna",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "kerjain aja yang bisa dikerjain",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Rahady Indra Wicaksono",
+        role: "student",
+        umur: 23,
+        "kalimat untuk hacktive": "coding adalah puzzle yang harus di selesaikan",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Eduardus Oldi Kristianto",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "hacktiv8 terlalu gg",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Muhammad Gilang Murdiyanto",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "olizwell",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Permana Putra",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "semakin maju",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Reza Adhitya Putra",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "belajar di hacktiv8",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Imam Khoiri",
+        role: "student",
+        umur: 23,
+        "kalimat untuk hacktive": "santuyin aja",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Dimas Nugroho Ramadhan",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "coding anti-Stress",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Willy Sandi Lious",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "bootcamp coding worthit",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Gusti Bisman Taka",
+        role: "student",
+        umur: 19,
+        "kalimat untuk hacktive": "coding anti-Stress",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Ahsan Al Fikri",
+        role: "student",
+        umur: 25,
+        "kalimat untuk hacktive": "bootcamp yang menyenangkan",
+        jenisKelamin: "Pria"
+    },
+    {
+        nama: "Pathrick Anthonio",
+        role: "Student",
+        umur: 27,
+        "kalimat untuk hacktive": "hackiv mania lanjut pantang ulang",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Teddy Juned",
+        role: "Instructor",
+        umur: 25,
+        "kalimat untuk hacktive": "ada naga dalam koding saya",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Zulkifli",
+        role: "Instructor",
+        umur: 19,
+        "kalimat untuk hacktive": "R",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Awalludin",
+        role: "Instructor",
+        umur: 29,
+        "kalimat untuk hacktive": "Waw",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Juan Sanjaya",
+        role: "Instructor",
+        umur: 22,
+        "kalimat untuk hacktive": "Hacktiv digoyang",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Dayat Hidayat",
+        role: "Student",
+        umur: 26,
+        "kalimat untuk hacktive": "B aja",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Adit Nuryana",
+        role: "Student",
+        umur: 25,
+        "kalimat untuk hacktive": "Perkuat pusat perbanyak cabang",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Safrizal Aji",
+        role: "Student",
+        umur: 21,
+        "kalimat untuk hacktive": "Bagus",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Excel",
+        role: "Student",
+        umur: 25,
+        "kalimat untuk hacktive": "Amazing",
+        jenisKelamin: "Pria",
+    },
+    {
+        nama: "Fikri Marzuki",
+        role: "Instructor",
+        umur: 28,
+        "kalimat untuk hacktive": "mantap",
+        jenisKelamin: "Pria",
+    },
+
+]
+// for (let i = 0; i < dbPria.length; i++) {
+//     const element = dbPria[i];
+//     console.log(element["kalimat untuk hacktive"]);
+// }
+
+const dbWanita = [
+    {
+        nama: "Ika Puspa Fairuz Wiwanata",
+        role: "student",
+        umur: 23,
+        "kalimat untuk hacktive": "Hacktiv8 Mantap Jaya",
+        jenisKelamin: "Wanita"
+    },
+    {
+        nama: "Devi Febrita",
+        role: "student",
+        umur: 24,
+        "kalimat untuk hacktive": "jangan lupa cari jodoh",
+        jenisKelamin: "Wanita"
+    },
+    {
+        nama: "Jeane Manuhutu",
+        role: "student",
+        umur: 23,
+        "kalimat untuk hacktive": "bersama teman-teman belajarnya",
+        jenisKelamin: "Wanita"
+    },
+    {
+        nama: "Rina Dahlia",
+        role: "student",
+        umur: 26,
+        "kalimat untuk hacktive": "jangan dibawa pusing",
+        jenisKelamin: "Wanita"
+    },
+    {
+        nama: "Nadya Rere",
+        role: "Student",
+        umur: 25,
+        "kalimat untuk hacktive": "selalu cayang hacktiv",
+        jenisKelamin: "Wanita",
+    },
+]
+
+
+module.exports = {
+    dbPria,
+    dbWanita,
+}
